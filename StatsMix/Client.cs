2ca9747390c7f5228b5e02eb91f6d0d02d7dcaf2
mcclaskc <christopher.m.mcclaskey@gmail.com>
2012-08-21 13:27:05 -0600

    added client and example class stubs

---
+++ StatsMix/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StatsMix
+{
+    class Client
+    {
+        public string ApiKey { get; set; }
+        const string BASE_URL = "http://statsmix.com/api/v2";
+
+        public Client(string api_key)
+        {
+            ApiKey = api_key;
+        }
+
+    }
+}
---
+++ StatsMix/Example.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StatsMix;
+
+namespace test
+{
+    class Example
+    {
+        const string YOUR_API_KEY = "abc123";
+        static void Main(string[] args)
+        {
+            Client smclient = new Client(YOUR_API_KEY);
+            string output = smclient.ApiKey;
+            System.Console.WriteLine("hello stats world, the api key is: " + output);
+            System.Console.ReadLine();
+        }
+    }
+}
