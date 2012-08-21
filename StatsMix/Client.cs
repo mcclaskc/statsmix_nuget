b85c907ec2a7fc145a8da2440d06175bc35d8dfc
mcclaskc <christopher.m.mcclaskey@gmail.com>
2012-08-21 16:54:11 -0600

    recieving successful responses from API, but not evident on site

--- StatsMix/Client.cs
+++ StatsMix/Client.cs
@@ -7,22 +7,27 @@ namespace StatsMix
     {
         public string ApiKey { get; set; }
         const string BaseUrl = "http://statsmix.com/api/v2";
+        RestClient restClient;
 
         public Client(string apiKey)
         {
             ApiKey = apiKey;
+            restClient = new RestClient(BaseUrl);
+            restClient.AddDefaultHeader("X-StatsMix-Token", apiKey);
         }
 
         public string track(string metricName, Stat stat)
         {
-            string parameters = "?name=" + metricName;
-            parameters += "&Value=" + stat.Value;
-            if (!stat.GeneratedAt.Equals(new DateTime())) parameters += "&GeneratedAt=" + stat.GeneratedAt;
+            var request = new RestRequest("track", Method.POST);
+            request.AddParameter("name", metricName); //string parameters = "?name=" + metricName;
+            request.AddParameter("value", stat.Value); //parameters += "&Value=" + stat.Value;
+            if (!stat.GeneratedAt.Equals(new DateTime())) request.AddParameter("generated_at", stat.GeneratedAt); //parameters += "&GeneratedAt=" + stat.GeneratedAt;
             //TODO META if (stat.Meta empty? stuff?) 
-            if (!string.IsNullOrEmpty(stat.RefId)) parameters += "&RefId=" + stat.RefId;
-            if (stat.ProfileId != 0) parameters += "&ProfileId=" + stat.ProfileId;
-
-            return BaseUrl + "/track" + parameters;
+            if (!string.IsNullOrEmpty(stat.RefId)) request.AddParameter("ref_id", stat.RefId);//parameters += "&RefId=" + stat.RefId;
+            if (stat.ProfileId != 0) request.AddParameter("profile_id", stat.ProfileId);//parameters += "&ProfileId=" + stat.ProfileId;
+            Console.WriteLine("REQUEST: " + request.Parameters);
+            return restClient.Execute(request).Content; //return BaseUrl + "/track" + parameters;
+     
         }
     }
 }
--- StatsMix/Example.cs
+++ StatsMix/Example.cs
@@ -1,28 +1,23 @@
 ﻿using System;
+using RestSharp;
 
 namespace test
 {
     class Example
     {
-        const string YOUR_API_KEY = "abc123";
+        const string YOUR_API_KEY = "d114e70073327345a717";
         static void Main(string[] args)
         {
-            StatsMix.Client smClient = new StatsMix.Client(YOUR_API_KEY);
-            string output = smClient.ApiKey;
-            Console.WriteLine("hello stats world, the api key is: " + output);
+            Console.WriteLine("hello stats world");
 
+            StatsMix.Client smClient = new StatsMix.Client(YOUR_API_KEY);
             StatsMix.Stat smStat = new StatsMix.Stat();
-            smStat.RefId = "0xDEADBEEF";
-           /*Console.WriteLine("Id: " + smStat.Id);
-            Console.WriteLine("Value: " + smStat.Value);
-            Console.WriteLine("Meta: " + smStat.Meta);
-            Console.WriteLine("RefId: " + smStat.RefId);
-            Console.WriteLine("ProfileId: " + smStat.ProfileId);
-            Console.WriteLine("MetricId: " + smStat.MetricId);
-            Console.WriteLine("GeneratedAt: " + smStat.GeneratedAt);
-            Console.WriteLine("CreatedAt: " + smStat.CreatedAt);
-            Console.WriteLine("UpdatedAt: " + smStat.UpdatedAt);*/
+            smStat.Value = 3;
+
+            Console.WriteLine("sending request...");
+            
             string result = smClient.track("metric_name", smStat);
+            
             Console.WriteLine("and your stat result is: " + result);
             Console.ReadLine();
         }
